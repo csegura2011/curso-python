35e9335d38979c98ab910bfe5acf63b5d9ac0b24
csegura2011 <cristian.segura.lepe@gmail.com>
2024-04-29 19:23:03 -0400

    actualizar contenidos

--- markdown/build.sh
+++ markdown/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-pandoc -i main.md -o main.pdf
+# pandoc -i main.md -o main.pdf
 
-pandoc *.md > markdown_book.html
+#pandoc *.md > markdown_book.html
+
+fname=main
+pandoc -s --toc -c pandoc.css -A footer.html --to=html5 ${fname}.md -o ${fname}.html
+
+
+
+
+
